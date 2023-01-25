e4f8272a7d2103c81e321c50667bb27911b5c038
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-01-25 14:31:25 +0000

    Write function to play a single round

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // use Math.random
 // Use console.log to test it works
 
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["Rock", "Paper", "Scissors"]
 
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * choices.length);
@@ -12,6 +12,24 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice())
+
+// write function to play a single round 
+// function takes 2 parameters, playerSelection and computerSelection
+// it returns a string announcing the winner
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "Paper") {
+        return "You lose! Paper beats Rock!";
+    } else {
+        return "Congratulations, you won!";
+    }
+  }
+  
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
 
 
 
